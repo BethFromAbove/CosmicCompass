1e9aaee8d53554f213eca17755910f818987db8f
Beth <bethcott@hotmail.co.uk>
2026-05-02 13:14:57 +0100

    Added astroquery test files

---
+++ astroqueryTest.py
@@ -0,0 +1,22 @@
+# sudo apt install python3-pip
+# pip3 install --pre --upgrade astroquery
+
+from astroquery.jplhorizons import Horizons
+mars = Horizons(id=499, location = '000', epochs=None, id_type=None)
+eph = mars.ephemerides()
+print(eph)
+
+print(eph['AZ'][0])
+print(eph['EL'][0])
+
+percentageArcAZ = (eph['AZ'][0])/360 #find Azimuth
+percentageArcEL = (eph['EL'][0])/360 #find Elevation
+
+print(percentageArcAZ)
+print(percentageArcEL)
+
+stepsNeededAZ = int(percentageArcAZ*512) #512 steps is 360degrees
+stepsNeededEL = int(percentageArcEL*512) #512 steps is 360degrees
+
+print(stepsNeededAZ)
+print(stepsNeededEL)
